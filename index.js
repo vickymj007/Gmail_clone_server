98ed6e1b8e106de209b9be16e9b7cb9fb47f3594
Vicky <vickypersish1997@gmail.com>
2024-04-13 15:45:33 +0530

    Bug resolved

--- index.js
+++ index.js
@@ -1,49 +1,54 @@
-const express = require("express")
-const cors = require('cors')
-const dotenv = require("dotenv")
-const mongoose = require('mongoose')
-const app = express()
-const userRoutes = require('./Routes/userRoutes.js')
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const app = express();
+const userRoutes = require("./Routes/userRoutes.js");
 
-dotenv.config()
+dotenv.config();
 
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
-    origin: true, 
-    credentials: true, 
-}))
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json(req);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
+app.use("/api/users", userRoutes);
+app.get("/verify-ip", async (req, res) => {
+  try {
+    res.status(200).json(req);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.get('/',(req,res)=>{
-    res.status(200).json({msg:"Welcome to the Gmail Clone app"})
-})
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL1);
 
+    console.log("Connected to MongoDB");
 
+    app.listen(process.env.PORT, () => {
+      console.log("Server is Listening");
+    });
+  } catch (error) {
+    console.log({
+      msg: "Unable to Connect to the Database",
+      error: error.message,
+    });
+    console.log(error);
+  }
+};
 
-app.use('/api/users', userRoutes)
-app.get("/verify-ip", (req, res) => {
-    try {
-      res.status(200).json(req);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
-
-const connectDB = async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URL1)
-
-        console.log("Connected to MongoDB");
-
-        app.listen(process.env.PORT,()=>{
-            console.log("Server is Listening");
-        })
-    } catch (error) {
-        console.log({msg:"Unable to Connect to the Database", error:error.message});
-        console.log(error);
-    }
-}
-
-connectDB()
+connectDB();
